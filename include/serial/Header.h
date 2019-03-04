546de6c2f9e530b708b71968cf34a8fae56f6998
Gida Pataki <gida.pataki@prezi.com>
2019-03-04 20:59:15 +0100

    Small fix

--- include/serial/Header.h
+++ include/serial/Header.h
@@ -5,6 +5,12 @@
 namespace serial {
 
 struct Header {
+	Header() = default;
+	Header(std::string doctype, int version)
+		: doctype(doctype)
+		, version(version)
+	{}
+
 	std::string doctype;
 	int version = 0;
 };
